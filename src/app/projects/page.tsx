dc13301a5332e4a9d431e63b3257bffb4192f61f
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-20 16:09:35 -0300

    feat: add page introduction component

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import { PageIntroduction } from "../components/pages/projects/page-introduction";
+
+export default function ProjectsPage() {
+    return (
+        <div>
+            <h1><PageIntroduction /></h1>
+        </div>
+    )
+}
