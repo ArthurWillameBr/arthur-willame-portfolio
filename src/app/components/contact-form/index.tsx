e572987a128825fec7ed8b93a4cb29683c8547ba
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-20 15:51:54 -0300

    feat: add contact form component

---
+++ src/app/components/contact-form/index.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { fadeUpAnimation } from "@/app/lib/animations";
+import { motion } from "framer-motion";
+import { Button } from "../button";
+import { SectionTitle } from "../section-title";
+import { ArrowRight } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+
+const contactFormData = z.object({
+  name: z.string().nonempty(),
+  email: z.string().email(),
+  message: z.string().nonempty(),
+})
+
+type ContactForm = z.infer<typeof contactFormData>
+
+export const ContactForm = () => {
+
+    const { register, handleSubmit, formState: { isSubmitting} } = useForm<ContactForm>({
+        resolver: zodResolver(contactFormData)
+    })
+
+    function onSubmit() {
+        console.log("ola")
+    }
+  return (
+    <section
+      className="py-16 px-6 md:py-32 flex items-center justify-center bg-zinc-950 bg-hero-image bg-cover bg-center bg-no-repeat"
+      id="contact"
+    >
+      <div className="w-full max-w-[420px] mx-auto">
+        <SectionTitle
+          subtitle="contato"
+          title="Vamos trabalhar juntos? Entre em contato"
+          className="items-center text-center"
+        />
+        <motion.form
+          className="mt-12 w-full flex flex-col gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+          {...fadeUpAnimation}
+        >
+          <input
+            placeholder="Nome"
+            className="w-full h-12 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600"
+            {...register("name")}
+          />
+          <input
+            placeholder="E-mail"
+            type="email"
+            className="w-full h-12 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600"
+            {...register("email")}
+          />
+          <textarea
+            placeholder="Mensagem"
+            className="resize-none w-full h-[138px] bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600"
+            {...register("message")}
+            maxLength={500}
+          />
+
+          <div className="relative w-max mx-auto mt-6">
+            <Button className="z-[2] relative" disabled={isSubmitting}>
+              Enviar mensagem
+              <ArrowRight size={18} />
+            </Button>
+            <div className="absolute inset-0 bg-violet-600 blur-2xl opacity-20" />
+          </div>
+        </motion.form>
+      </div>
+    </section>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/header";
+import { ContactForm } from "./components/contact-form";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <Header/> 
         {children}
+        <ContactForm />
         </body>
     </html>
   );
