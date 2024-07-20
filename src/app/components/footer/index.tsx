904a5eda0bdff2c3335c28e5261af271ed800b88
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-20 16:02:59 -0300

    feat: update footer component with animated heart icon

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,11 +1,11 @@
-import { Heart } from "lucide-react"
+import { HeartPulse } from "lucide-react"
 
 export const Footer = () => {
   return (
-    <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
-      <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
+    <footer className="h-14 w-full flex items-center justify-center bg-zinc-900/30">
+      <span className="flex items-center gap-1.5 text-xs sm:text-sm text-gray-400">
         Made with
-        <Heart size={13} className="text-violet-500" />
+        <HeartPulse size={13} className="text-violet-500" />
         by
         <strong className="font-medium">Arthur Willame</strong>
       </span>
