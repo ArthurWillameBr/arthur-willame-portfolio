1c445defdeb3deada071f3b24dd96bd1f88e2972
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-22 19:18:21 -0300

    chore: Update env.ts to use new variable names for hygraph URL and token

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
     NEXT_PUBLIC_WEBHOOK_URL: z.string().url(),
-    NEXT_PUBLIC_HYGRAPH_URL: z.string().url(),
-    NEXT_PUBLIC_HYGRAPH_TOKEN: z.string(),
+    HYGRAPH_URL: z.string().url(),
+    HYGRAPH_TOKEN: z.string(),
 })
-
-console.log("NEXT_PUBLIC_WEBHOOK_URL", process.env.NEXT_PUBLIC_WEBHOOK_URL)
 
 export const env = envSchema.parse(process.env)
