555e5335fa9c3d656b0f16a66e85356291a8f6a4
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-22 18:47:46 -0300

    feat: Update KnowTechs component to use CMSIcon for tech icons

--- src/app/components/pages/home/know-techs/know-tech.tsx
+++ src/app/components/pages/home/know-techs/know-tech.tsx
@@ -1,22 +1,19 @@
-'use client'
+"use client";
 
-import Image from "next/image"
+import { CMSIcon } from "@/app/components/cms-icon";
+import { KnownTech as IKnownTech } from "@/types/projects";
 
 type KnownTechProps = {
-  tech: {
-    url: string,
-    name: string,
-  }
-}
+  tech: IKnownTech;
+};
 
 export const KnownTech = ({ tech }: KnownTechProps) => {
   return (
     <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-violet-500 hover:bg-gray-600/30 transition-all">
-      <div className="flex items-center justify-between">
+      <div className="h-28 flex items-center justify-between">
         <p className="font-medium text-lg">{tech.name}</p>
-        <Image src={tech.url} width={42}
-          height={34} alt={tech.name}/>
+          <CMSIcon icon={tech.iconSvg} />
       </div>
     </div>
-  )
-}
+  );
+};
