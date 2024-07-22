0d1a6df8fe4eee4d08f7d6fbb4cdfaed76ceb135
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-22 18:47:24 -0300

    feat: Update KnowTechs component to accept techs prop

--- src/app/components/pages/home/know-techs/index.tsx
+++ src/app/components/pages/home/know-techs/index.tsx
@@ -1,16 +1,19 @@
 import { SectionTitle } from "@/app/components/section-title";
 import { KnownTech } from "./know-tech";
+import { KnownTech as IKnownTech } from "@/types/projects";
 
-export const KnowTechs = () => {
+
+interface KnowTechsProps {
+  techs: IKnownTech[],
+}
+
+export const KnowTechs = ({techs}: KnowTechsProps) => {
   return (
     <section className="container py-14">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
       <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-10">
-          {Array.from({ length: 6 }).map((_, i) => (
-              <KnownTech key={i} tech={{
-                url: "/NextJS-Dark.svg",
-                name: "Next.js",
-            }} />
+          {techs.map((tech) => (
+              <KnownTech key={tech.name} tech={tech} />
           ) )}
       </div>
     </section>
