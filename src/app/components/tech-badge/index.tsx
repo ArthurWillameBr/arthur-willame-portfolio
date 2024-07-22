445c0651fd8f4b4c6f4bd514f5f88da560052c18
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-22 19:16:54 -0300

    feat: Update TechBadge component to use white background color

--- src/app/components/tech-badge/index.tsx
+++ src/app/components/tech-badge/index.tsx
@@ -10,7 +10,7 @@ type TechBadgeProps = ComponentProps<typeof motion.span> & {
 export const TechBadge = ({ name, ...props }: TechBadgeProps) => {
   return (
     <motion.span
-      className="text-violet-300 bg-violet-900/80 text-sm py-1 px-3 rounded-lg"
+      className="text-white bg-violet-800/80 text-sm py-1 px-3 rounded-lg"
       {...props}
     >
       {name}
