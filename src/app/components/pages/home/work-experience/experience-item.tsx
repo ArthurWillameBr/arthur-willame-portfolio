c9c2e3cd082b2e1f78e395291b7ea639ae7601a7
arthurwillame5 <arthurwillame2005@gmail.com>
2024-07-23 21:46:12 -0300

    feat: Update ExperienceItem component to display work experience details

--- src/app/components/pages/home/work-experience/experience-item.tsx
+++ src/app/components/pages/home/work-experience/experience-item.tsx
@@ -1,62 +1,107 @@
-"use client"
+"use client";
 
-import { TechBadge } from "@/app/components/tech-badge"
-import { fadeUpAnimation, techBadgeAnimation } from "@/app/lib/animations"
-import { motion } from "framer-motion"
-import Image from "next/image"
+import { TechBadge } from "@/app/components/tech-badge";
+import { fadeUpAnimation, techBadgeAnimation } from "@/app/lib/animations";
+import { WorkExperience } from "@/types/work-experiencie";
+import { RichText } from "@graphcms/rich-text-react-renderer";
+import { differenceInMonths, differenceInYears } from "date-fns";
+import { format } from "date-fns/format";
+import { ptBR } from "date-fns/locale";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
-
-export const ExperienceItem = () => {
-    return (
-        <motion.div
-        className="grid grid-cols-[40px,1fr] gap-4 md:gap-10"
-        {...fadeUpAnimation}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="flex items-center flex-col gap-4">
-          <div className="rounded-full border border-gray-500 p-0.5">
-            <Image
-              src={"/Docker.svg"}
-              width={40}
-              height={40}
-              className="rounded-full"
-              alt={`Logo da empresa`}
-            />
-          </div>
-  
-          <div className="h-full w-[1px] bg-gray-800" />
-        </div>
-  
-        <div>
-          <div className="flex flex-col gap-2 text-sm sm:text-base">
-            <a
-              href={"https://www.google.com"}
-              target="_blank"
-              className="text-gray-500 hover:text-emerald-500 transition-colors"
-              rel="noreferrer"
-            >
-              @ UFC
-            </a>
-            <h4 className="text-gray-300">Programador Frontend</h4>
-            <span className="text-gray-500">
-             jan 2024 • O momento • (6 meses)
-            </span>
-            <div className="text-gray-400">
-             Rich Text descption
-            </div>
-          </div>
-  
-          <p className="text-gray-400 text-sm mb-3 mt-6 font-semibold">
-            Competência
-          </p>
-          <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
-              <TechBadge
-                name={"React.js"}
-                {...techBadgeAnimation}
-                transition={{ duration: 0.2}}
-              />
-          </div>
-        </div>
-      </motion.div>
-    )
+interface ExperienceItemProps {
+  experience: WorkExperience;
 }
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+  const {
+    endDate,
+    companyLogo,
+    companyName,
+    companyUrl,
+    description,
+    role,
+    technologies
+  } = experience;
+
+  const startDate = new Date(experience.startDate)
+
+  const formattedStartDate = format(startDate, "MMM yyyy", { locale: ptBR} )
+
+  const formattedEndDate = endDate ? format(new Date(endDate), "MMM yyyy", {locale: ptBR}) : "O momento"
+
+  const end = endDate ? new Date(endDate) : new Date()
+
+  const months = differenceInMonths(end, startDate)
+
+  const years = differenceInYears(end, startDate)
+
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+  years > 0
+    ? `${years} ano${years > 1 ? 's' : ''}${
+        monthsRemaining > 0
+          ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+          : ''
+      }`
+    : `${months} mes${months > 1 ? 'es' : ''}`
+
+
+  return (
+    <motion.div
+      className="grid grid-cols-[40px,1fr] gap-4 md:gap-10"
+      {...fadeUpAnimation}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="flex items-center flex-col gap-4">
+        <div className="rounded-full border border-gray-500 p-0.5">
+          <Image
+            src={companyLogo.url}
+            width={40}
+            height={40}
+            className="rounded-full"
+            alt={companyName}
+          />
+        </div>
+
+        <div className="h-full w-[1px] bg-gray-800" />
+      </div>
+
+      <div>
+        <div className="flex flex-col gap-2 text-sm sm:text-base">
+          <a
+            href={companyUrl}
+            target="_blank"
+            className="text-gray-500 hover:text-violet-500 transition-colors"
+            rel="noreferrer"
+          >
+            @ {companyName}
+          </a>
+          <h4 className="text-gray-300">{role}</h4>
+          <span className="text-gray-500">
+            {formattedStartDate} • {formattedEndDate} • ({formattedDuration})
+          </span>
+          <div className="text-gray-400">
+            <RichText content={description.raw} />
+          </div>
+        </div>
+
+        <p className="text-gray-400 text-sm mb-3 mt-6 font-semibold">
+          Competência
+        </p>
+        <div className="flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8">
+          {technologies.map((tech) => (
+            <TechBadge
+              key={tech.name}
+              name={tech.name}
+              {...techBadgeAnimation}
+              transition={{ duration: 0.2 }}
+            />
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
