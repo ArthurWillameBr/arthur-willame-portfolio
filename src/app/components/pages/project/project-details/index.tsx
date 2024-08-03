6e04221726ec837e36268f2c603910c105679c78
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-02 21:47:54 -0300

    feat: add project details page

--- src/app/components/pages/project/project-details/index.tsx
+++ src/app/components/pages/project/project-details/index.tsx
@@ -1,14 +1,20 @@
-'use client'
+"use client";
 
-import { Button } from '@/app/components/button'
-import { Link } from '@/app/components/link'
-import { SectionTitle } from '@/app/components/section-title'
-import { TechBadge } from '@/app/components/tech-badge'
-import { motion } from 'framer-motion'
-import { fadeUpAnimation, techBadgeAnimation } from '@/app/lib/animations'
-import { ArrowLeft, Github, Link2 } from 'lucide-react'
+import { Button } from "@/app/components/button";
+import { Link } from "@/app/components/link";
+import { SectionTitle } from "@/app/components/section-title";
+import { TechBadge } from "@/app/components/tech-badge";
+import { motion } from "framer-motion";
+import { fadeUpAnimation, techBadgeAnimation } from "@/app/lib/animations";
+import { ArrowLeft, Github, Link2 } from "lucide-react";
+import { Project } from "@/types/projects";
+import { RichText } from "@/app/components/rich-text";
 
-export const ProjectDetails = () => {
+interface ProjectDetailsProps {
+  project: Project;
+}
+
+export const ProjectDetails = ({ project }: ProjectDetailsProps) => {
   return (
     <section className="w-full sm:min-h-[750px] flex flex-col items-center justify-center relative pb-10 sm:pb-24 py-24 px-6 overflow-hidden">
       <motion.div
@@ -23,63 +29,43 @@ export const ProjectDetails = () => {
 
       <SectionTitle
         subtitle="projetos"
-        title={"ConnectNotes"}
+        title={project.title}
         className="text-center items-center sm:[&>h3]:text-4xl"
       />
       <motion.div
         className="text-gray-400 text-center max-w-[640px] my-4 sm:my-6 text-sm sm:text-base"
         {...fadeUpAnimation}
       >
-        Rich text description
+        <RichText content={project.description.raw} />
       </motion.div>
       <div className="w-full max-w-[330px] flex flex-wrap gap-2 items-center justify-center">
-          <TechBadge
-            name={"Next.js"}
-            {...techBadgeAnimation}
-            transition={{ duration: 0.3}}
-          />
-          <TechBadge
-            name={"Next.js"}
-            {...techBadgeAnimation}
-            transition={{ duration: 0.3}}
-          />
-          <TechBadge
-            name={"Next.js"}
-            {...techBadgeAnimation}
-            transition={{ duration: 0.3}}
-          />
-          <TechBadge
-            name={"Next.js"}
-            {...techBadgeAnimation}
-            transition={{ duration: 0.3}}
-          />
-          <TechBadge
-            name={"Next.js"}
-            {...techBadgeAnimation}
-            transition={{ duration: 0.3}}
-          />
+        {project.technologies.map((tech) => (
+          <TechBadge key={tech.name} name={tech.name} {...techBadgeAnimation} />
+        ))}
       </div>
       <motion.div
         className="my-6 sm:my-12 flex items-center gap-2 sm:gap-4 flex-col sm:flex-row"
         {...fadeUpAnimation}
       >
-          <a href="#" target="_blank" rel="noreferrer">
-            <Button className="min-w-[180px]">
-              <Github size={20} />
-              Repositório
-            </Button>
-          </a>
-          <a href="#" target="_blank" rel="noreferrer">
+        <a href={project.githubUrl} target="_blank" rel="noreferrer">
+          <Button className="min-w-[180px]">
+            <Github size={20} />
+            Repositório
+          </Button>
+        </a>
+        {project?.liveProjectUrl && (
+          <a href={project.liveProjectUrl} target="_blank" rel="noreferrer">
             <Button className="min-w-[180px]">
               <Link2 size={20} />
               Projeto Online
             </Button>
           </a>
+        )}
       </motion.div>
       <Link href="/projects">
         <ArrowLeft size={20} />
         Voltar para projetos
       </Link>
     </section>
-  )
-}
+  );
+};
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,10 +1,56 @@
 import { ProjectDetails } from "@/app/components/pages/project/project-details";
 import { ProjectSections } from "@/app/components/pages/projects/project-sections";
+import { ProjectPageData } from "@/types/page-info";
+import { fetchHygraphQuery } from "@/utils/fetch-hygraph-query";
 
-export default function Project() {
+interface ProjectPageProps {
+  params: {
+    slug: string
+  }
+}
+
+const getProjectDetails = async (slug: string): Promise<ProjectPageData> => {
+  const query = `
+    query project {
+    project(where: {slug: "${slug}"}) {
+    pageThumbnail {
+      url
+    }
+      thumbnail {
+        url
+      }
+      sections {
+        title
+        image {
+          url
+        }
+      }
+      title
+      shortDescription
+      description {
+        raw
+        text
+      }
+      technologies {
+        name
+      }
+      liveProjectUrl
+      githubUrl
+    }
+  }
+  `;
+  return fetchHygraphQuery(
+    query,
+    60 * 60 * 24 // 1 day
+  );
+};
+export default async function Project({params: {slug} }:ProjectPageProps) {
+  const {project} = await getProjectDetails(slug);
+
+
   return (
     <>
-      <ProjectDetails />
+      <ProjectDetails project={project} />
       <ProjectSections />
     </>
   );
