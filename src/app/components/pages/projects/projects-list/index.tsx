d53e2d8c833353b9b9ccfc49fb31503d6a3577d0
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-04 21:28:45 -0300

    feat: Update ProjectsList component to include fade animation for project cards

--- src/app/components/pages/projects/projects-list/index.tsx
+++ src/app/components/pages/projects/projects-list/index.tsx
@@ -13,13 +13,18 @@ interface ProjectsListProps {
 export const ProjectsList = ({ projects }: ProjectsListProps) => {
   return (
     <section className="container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
-      <motion.div {...fadeUpAnimation} transition={{ duration: 0.5 }}>
-        {projects.map((project) => (
+      {projects.map((project, index) => (
+        <motion.div
+          key={index}
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+        >
           <Link key={project.title} href={`/projects/${project.slug}`}>
             <ProjectCard project={project} />
           </Link>
-        ))}
-      </motion.div>
+        </motion.div>
+      ))}
     </section>
   );
 };
+;
